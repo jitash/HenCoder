b72e64369fc1ec8becba70a02644e38f55e60e2a
jitash <jitash@foxmail.com>
2018-08-29 21:05:14 +0800

    no message

---
+++ PracticeDraw1/app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
@@ -0,0 +1,49 @@
+package com.hencoder.hencoderpracticedraw1.practice;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Practice2DrawCircleView extends View {
+private Paint mPaint;
+    public Practice2DrawCircleView(Context context) {
+        super(context);
+    }
+
+    public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+//        练习内容：使用 canvas.drawCircle() 方法画圆
+//        一共四个圆：1.实心圆 2.空心圆 3.蓝色实心圆 4.线宽为 20 的空心圆
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLACK);
+        
+        
+        canvas.drawCircle(200,200,100,mPaint);
+        
+        mPaint.setStyle(Paint.Style.STROKE);
+        canvas.drawCircle(500,200,100,mPaint);
+        
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setColor(Color.BLUE);
+        canvas.drawCircle(200,500,100,mPaint);
+        
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStrokeWidth(20);
+        canvas.drawCircle(500,500,100,mPaint);
+    }
+}
---
+++ PracticeDraw1/app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
@@ -0,0 +1,47 @@
+package com.hencoder.hencoderpracticedraw1.practice;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Practice9DrawPathView extends View {
+
+    public Practice9DrawPathView(Context context) {
+        super(context);
+    }
+
+    public Practice9DrawPathView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice9DrawPathView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+//        练习内容：使用 canvas.drawPath() 方法画心形
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+
+        Path path = new Path();
+        RectF rectF = new RectF(100,50,150,150);
+        path.addArc(rectF,-180,180);
+
+        RectF rectF2 = new RectF(150,50,200,150);
+        path.addArc(rectF,-180,180);
+        path.moveTo(100, 100);
+        path.lineTo(150, 150);
+        path.lineTo(200, 100);
+        canvas.drawPath(path, paint);
+//        path.setFillType(Path.FillType.WINDING);
+        
+    }
+}
